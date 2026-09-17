a85bea4f71f58a7346011e26fff1c05ae51c0031
Montreal2025 <brayanmakalister2023@gmail.com>
2026-09-17 02:33:50 -0600

    intentemos

--- lavanderia-salinas/src/components/AppShell.vue
+++ lavanderia-salinas/src/components/AppShell.vue
@@ -78,8 +78,8 @@
                   <ion-icon :icon="logoFacebook" />
                 </button>
                 <button class="banner-perfil-superior" type="button" title="Editar mi perfil" aria-label="Editar mi perfil" :disabled="!esAdministrador && funcionesBloqueadas" @click="abrirPerfil">
-                  <img v-if="esUsuarioDesarrollador" :src="imagenDesarrollador" :alt="`Foto de ${usuarioActual.nombre}`" />
-                  <img v-else-if="usuarioActual?.imagenPerfil" :src="usuarioActual.imagenPerfil" :alt="`Foto de ${usuarioActual.nombre}`" />
+                  <img v-if="esUsuarioDesarrollador" :src="imagenDesarrollador" :alt="`Foto de ${usuarioActual?.nombre || 'Usuario'}`" />
+                  <img v-else-if="usuarioActual?.imagenPerfil" :src="usuarioActual.imagenPerfil" :alt="`Foto de ${usuarioActual?.nombre || 'Usuario'}`" />
                   <ion-icon v-else :icon="personCircleOutline" />
                 </button>
               </div>
@@ -192,8 +192,8 @@
                   <ion-icon :icon="logoFacebook" />
                 </button>
                 <button class="banner-perfil-superior banner-perfil-superior-compacto" type="button" title="Editar mi perfil" aria-label="Editar mi perfil" :disabled="!esAdministrador && funcionesBloqueadas" @click="abrirPerfil">
-                  <img v-if="esUsuarioDesarrollador" :src="imagenDesarrollador" :alt="`Foto de ${usuarioActual.nombre}`" />
-                  <img v-else-if="usuarioActual?.imagenPerfil" :src="usuarioActual.imagenPerfil" :alt="`Foto de ${usuarioActual.nombre}`" />
+                  <img v-if="esUsuarioDesarrollador" :src="imagenDesarrollador" :alt="`Foto de ${usuarioActual?.nombre || 'Usuario'}`" />
+                  <img v-else-if="usuarioActual?.imagenPerfil" :src="usuarioActual.imagenPerfil" :alt="`Foto de ${usuarioActual?.nombre || 'Usuario'}`" />
                   <ion-icon v-else :icon="personCircleOutline" />
                 </button>
               </div>
@@ -2149,11 +2149,11 @@ const descargarReporteCierre = (cierre: any) => {
     y += 9
   }
 
-  const dibujarCard = (etiqueta: string, valor: string, color: { fondo: number[], texto: number[] }, x: number, anchoCard: number) => {
+  const dibujarCard = (etiqueta: string, valor: string, color: { fondo: [number, number, number], texto: [number, number, number] }, x: number, anchoCard: number) => {
     const altoCard = 20
-    pdf.setFillColor(...color.fondo)
+    pdf.setFillColor(color.fondo[0], color.fondo[1], color.fondo[2])
     pdf.roundedRect(x, y, anchoCard, altoCard, 3, 3, 'F')
-    pdf.setTextColor(...color.texto)
+    pdf.setTextColor(color.texto[0], color.texto[1], color.texto[2])
     pdf.setFont('helvetica', 'normal')
     pdf.setFontSize(7)
     pdf.text(etiqueta.toUpperCase(), x + 4, y + 7)
